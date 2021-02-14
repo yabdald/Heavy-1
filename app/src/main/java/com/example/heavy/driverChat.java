ed3826d9d71b1befbc1b44a8edbc1b3a35b2cf3e
yabdald <yabdald5@gmail.com>
2021-02-14 04:04:32 +0400

    ..

---
+++ app/src/main/java/com/example/heavy/driverChat.java
@@ -0,0 +1,14 @@
+package com.example.heavy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class driverChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_chat);
+    }
+}
